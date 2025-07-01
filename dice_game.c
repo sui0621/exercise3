31e2e83fe8d253422e38c8defe40e7c545404eea
sui0621 <mokkeobake@icloud.com>
2025-07-01 16:02:23 +0900

    Greetings added

--- dice_game.c
+++ dice_game.c
@@ -6,8 +6,12 @@ int main(){
 int dice1;
 int dice2;
 int add;
+char name[10];
 
 srand((unsigned int)time(NULL));
+printf("What is your name?\n");
+scanf("%s",name);
+printf("Hello,%s!\n",name);
 
 printf("Rolling dice...\n");
     dice1 = rand()%6+1;
@@ -16,4 +20,6 @@ printf("Rolling dice...\n");
     printf("Die 2: %d\n",dice2);
     add = dice1 + dice2;
     printf("Total value: %d\n",add);
+
+
 }
