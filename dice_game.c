a48c3042a4ebda34d6d9bc7f01543b056c270a25
sui0621 <mokkeobake@icloud.com>
2025-07-01 17:06:18 +0900

    Won/lost message modified

--- dice_game.c
+++ dice_game.c
@@ -21,6 +21,6 @@ printf("Rolling dice...\n");
     add = dice1 + dice2;
     printf("Total value: %d\n",add);
 
-    if(add > 7) printf("You won\n");
-    else printf("You lost\n");
+    if(add > 7) printf("%s won\n",name);
+    else printf("%s lost\n",name);
 }
